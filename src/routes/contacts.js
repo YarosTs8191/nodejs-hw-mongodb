2c4f21708e7db6630a7a02cb87d07b9b7c230b89
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-07-05 12:45:18 +0200

    imports-corrects

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import {
-  getAllContacts,
-  getContactById,
-  createContact,
+  getAllContactsController,
+  getContactByIdController,
 } from '../controllers/contactsController.js';
 
 const router = express.Router();
 
-router.get('/', getAllContacts);
-router.get('/:contactId', getContactById);
-router.post('/', createContact);
+router.get('/', getAllContactsController);
+router.get('/:contactId', getContactByIdController);
 
 export default router;
