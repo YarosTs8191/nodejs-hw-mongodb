b3884663d1712f554021a242706d3dbb6fc407b5
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-07-17 16:00:15 +0200

    fin-prod

--- src/models/contact.js
+++ src/models/contact.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
+    name: { type: String, required: true, minlength: 3, maxlength: 20 },
+    phoneNumber: { type: String, required: true, minlength: 3, maxlength: 20 },
     email: String,
     isFavourite: { type: Boolean, default: false },
     contactType: {
