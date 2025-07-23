f97c92d93f107c0b17ee268b6930986f39155a44
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-07-23 18:00:57 +0200

    cockies-add

--- src/server.js
+++ src/server.js
@@ -5,16 +5,18 @@ import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import authRouter from './routers/auth.js';
+import cookieParser from 'cookie-parser';
 
 export function setupServer() {
   const app = express();
   app.use(cors());
   app.use(pino());
   app.use(express.json());
+  app.use(cookieParser());
   app.use('/contacts', contactsRouter);
+  app.use('/auth', authRouter);
   app.use(notFoundHandler);
   app.use(errorHandler);
-  app.use('/auth', authRouter);
 
   app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
