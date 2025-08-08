d0420ac54c9f11aaf1329c15750341e67850472e
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-08-09 00:48:59 +0200

    auth-swager

--- src/server.js
+++ src/server.js
@@ -6,6 +6,16 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import authRouter from './routers/auth.js';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerPath = path.join(__dirname, '../docs/swagger.json');
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'));
 
 export function setupServer() {
   const app = express();
@@ -15,6 +25,7 @@ export function setupServer() {
   app.use(cookieParser());
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use(notFoundHandler);
   app.use(errorHandler);
 
